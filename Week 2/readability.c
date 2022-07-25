3dee00cb5e4b948c701bdc67496dc1a44d09f390
electrovoid <82676783+electrovoid@users.noreply.github.com>
2022-07-25 20:19:31 +0200

    Readability
    Problem Set 2: Readability

--- Week 2/readability.c
+++ Week 2/readability.c
@@ -12,14 +12,14 @@ int main(void) {
     
     str = get_string("Text: ");
     
-    for(int i = 0; i < strlen(str); i++) {
-        if(str[i] == '.' || str[i] == '!' || str[i] == '?') {
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] == '.' || str[i] == '!' || str[i] == '?') {
             sentences++;
         }
-        else if(str[i] == ' ') {
+        else if (str[i] == ' ') {
             words++;
         }
-        else if((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')) {
+        else if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')) {
             letters++;
         }
     }
@@ -29,10 +29,10 @@ int main(void) {
     
     float index = 0.0588*L - 0.296*S - 15.8;
     
-    if(index < 1) {
+    if (index < 1) {
         printf("Before Grade 1\n");
     }
-    else if(index > 16) {
+    else if (index > 16) {
         printf("Grade 16+\n");
     }
     else {
