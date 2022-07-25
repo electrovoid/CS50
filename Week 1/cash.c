f21a74aa420ee045169920836c85415affaaf965
electrovoid <82676783+electrovoid@users.noreply.github.com>
2022-07-25 20:14:07 +0200

    Cash
    Problem Set 1: Cash

--- Week 1/cash.c
+++ Week 1/cash.c
@@ -13,20 +13,20 @@ int main(void) {
     
     int cents = round(dollars*100);
     
-    while(cents > 0) {
-        if(cents >= 25) {
+    while (cents > 0) {
+        if (cents >= 25) {
             cents = cents-25;
             count++;
         }
-        else if(cents >= 10 && cents < 25) {
+        else if (cents >= 10 && cents < 25) {
             cents = cents-10;
             count++;
         }
-        else if(cents >= 5 && cents < 10) {
+        else if (cents >= 5 && cents < 10) {
             cents = cents-5;
             count++;
         }
-        else if(cents >= 1 && cents < 5) {
+        else if (cents >= 1 && cents < 5) {
             cents = cents-1;
             count++;
         }
